0b76328d98ce5ea36a8e72aa8824e7421f84aa34
Dustin Hershman <dustinh17@gmail.com>
2021-04-30 13:06:26 -0400

    give typings an export

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,10 +2,13 @@ declare let D: D.Static
 
 declare namespace D {
   type KeyValuePair<K, V> = [K, V]
-  type Mappable = Array<KeyValuePair<any, any>>
   type Pred = (...a: any[]) => boolean
+  type Mappable = Array<KeyValuePair<any, any>>
 
   interface Static {
 
   }
 }
+
+export = D
+export as namespace D
