543a4be45e9cb8df3a8503ed4c7b99267957ab2b
Dustin Hershman <dustinh17@gmail.com>
2021-05-03 14:04:25 -0400

    compare qty util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { createError, ErrorCodes } from 'olive-data-contracts'
+
 /**
  * Takes in a string and returns it back in camel case
  * @param {String} str The string we want to camel case
@@ -9,6 +11,27 @@ function toCamelCase (str) {
   return text.substr(0, 1).toLowerCase() + text.substr(1)
 }
 
+/**
+ * Compares quantity between procedure codes, and a cpt, based on a generic object
+ * @param {Array} procs An Array of ProcedureCodes to search through
+ * @param {Object} cpt A CPT object to use
+ * @returns An Array of errors, or an empty array
+ */
+function compareQty (procs, cpt) {
+  const found = procs.find(({ value }) => value === cpt.code)
+
+  if (!found?.quantity) {
+    return []
+  }
+
+  if (Number(found?.quantity) !== Number(cpt.approvedUnits)) {
+    return [createError(ErrorCodes.AUTH_MISMATCHED_PROCEDURE_CODE_QUANTITY)]
+  }
+
+  return []
+}
+
 export default {
+  compareQty,
   toCamelCase
 }
--- types/src/utils.d.ts
+++ types/src/utils.d.ts
@@ -1,4 +1,14 @@
+import { GlobalError, ProcedureCode } from 'olive-data-contracts'
+
+interface PortalCode {
+  modifier?: string
+  serviceUnits?: string
+  approvedUnits?: string
+  code: string
+  description: string
+}
 
 export interface Utils {
+  compareQty(procs: ProcedureCode[], cpt: PortalCode): GlobalError[]
   toCamelCase(str: string): string
 }
